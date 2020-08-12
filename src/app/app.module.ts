db843c2f5b9e71e6ac01960d55ff5ac55c36a0b8
SaraUP <sarisup14@gmail.com>
2020-08-12 15:50:50 -0400

    Tarea 1: 08/12/20 --> module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
-import { PersonaComponent } from './test/persona/persona.component';
+import { FormsModule } from '@angular/forms';
 
+import { PersonaModule } from './modules/persona/persona.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PersonaComponent
+    //Componentes y directivas
+    AppComponent
   ],
   imports: [
+    //Modulos dentro de este modulo
     BrowserModule,
-    FormsModule
+    FormsModule,
+    PersonaModule
   ],
-  providers: [],
+  providers: [
+    //Servicios
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
