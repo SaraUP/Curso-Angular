cdf564c65ee6f18cd432930578aa4a7b1252eedd
SaraUP <sarisup14@gmail.com>
2020-08-17 15:46:39 -0400

    Tarea 2: 08/17/20 --> first directive improved

--- src/app/directives/ng-color.directive.ts
+++ src/app/directives/ng-color.directive.ts
@@ -1,14 +1,31 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appNgColor]'
 })
 export class NgColorDirective {
 
-  constructor(el: ElementRef) {
+  @HostListener('mouseenter') onMouseEnter(){
+    console.log('Mouse enter');
+    //this.el.nativeElement.style.backgroundColor = "red";
+    this.changeColor("yellow");
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    console.log('Mouse leave');
+    //this.el.nativeElement.style.backgroundColor = "red";
+    this.changeColor("red");
+  }
+
+  constructor(private el: ElementRef) {
     console.log('Element ref ', el)
 
-    el.nativeElement.style.backgroundColor = "red"; 
+    //this.el.nativeElement.style.backgroundColor = "red";
+    this.changeColor("red"); 
+  }
+
+  changeColor(color: string){
+    this.el.nativeElement.style.backgroundColor = color;
   }
 
 }
