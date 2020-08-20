e50a05b4dbfc51cc1abe348e9e72a24743d9ca06
SaraUP <sarisup14@gmail.com>
2020-08-20 16:17:14 -0400

    Tarea 1: 08/20/20 -->  first routing arreglos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { AboutComponent } from './components/about/about.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
-import { AppRoutingModule } from './app.routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
