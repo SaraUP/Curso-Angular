028a6d787cefb15aec3f50d4cdb095084185abca
SaraUP <sarisup14@gmail.com>
2020-08-21 15:39:24 -0400

    Tarea 1: 08/21/20 -->  first child router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AboutComponent } from './components/about/about.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Home1Component } from './components/home1/home1.component';
+import { Home2Component } from './components/home2/home2.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { AppRoutingModule } from './app-routing.module';
     AppComponent,
     AboutComponent,
     AdminComponent,
-    HomeComponent
+    HomeComponent,
+    Home1Component,
+    Home2Component
   ],
   imports: [
     //Modulos dentro de este modulo
