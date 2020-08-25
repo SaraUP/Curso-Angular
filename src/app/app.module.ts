96fbd90a2f1dd0844dbbb6114b0fc4ae7ae91d59
SaraUP <sarisup14@gmail.com>
2020-08-25 16:10:52 -0400

    Adding routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,11 @@ import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { MatSliderModule } from "@angular/material/slider";
 import { MatIconModule } from "@angular/material/icon";
+
 
 @NgModule({
   declarations: [
