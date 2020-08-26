a4561b234ab309709281b7cc6e41ffbf58d2e2e5
SaraUP <sarisup14@gmail.com>
2020-08-26 16:01:21 -0400

    login + pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { MatSliderModule } from "@angular/material/slider";
 import { MatIconModule } from "@angular/material/icon";
+import { PagesComponent } from './pages/pages.component';
 
 
 @NgModule({
   declarations: [
     //Componentes y directivas
-    AppComponent
+    AppComponent,
+    PagesComponent
   ],
   imports: [
     //Modulos dentro de este modulo
