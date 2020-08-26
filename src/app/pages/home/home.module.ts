4906780cd2060803a6ca5a13b2b347591bf0c90c
SaraUP <sarisup14@gmail.com>
2020-08-26 15:16:04 -0400

    Tarea 1: card angular material in home module

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatCardModule,
+    MatButtonModule
   ],
   declarations: [
     HomeComponent
