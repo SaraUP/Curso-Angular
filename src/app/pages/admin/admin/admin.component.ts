6ded7b6a73e35b87ecc5ced6392b7d21b1ee347d
SaraUP <sarisup14@gmail.com>
2020-09-01 16:10:33 -0400

    Tarea 2: 09/01/20 --> template and reactive forms

--- src/app/pages/admin/admin/admin.component.ts
+++ src/app/pages/admin/admin/admin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -16,11 +16,11 @@ export class AdminComponent implements OnInit {
 
   ngOnInit() {
     this.productForm = this.formBuilder.group({
-      description:'',
-      imageURL: '',
-      ownerId: '',
-      price: '',
-      title: ''
+      description:['description', [Validators.required, Validators.minLength(3)]],
+      imageURL: 'image',
+      ownerId: 'owner',
+      price: 'price',
+      title: 'title'
     });
   }
 
