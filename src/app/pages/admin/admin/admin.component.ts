7bfb9ff35d59e4283d9ee1df0920042418c546fa
SaraUP <sarisup14@gmail.com>
2020-09-02 15:45:23 -0400

    Tarea 2: 09/02/20 --> Unsubscription

--- src/app/pages/admin/admin/admin.component.ts
+++ src/app/pages/admin/admin/admin.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ProductService } from '../../../services/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent implements OnInit, OnDestroy {
 
   //nameControl = new FormControl();
 
   productForm: FormGroup;
+
+  productSubs: Subscription;
 
   constructor(private formBuilder: FormBuilder, private productService: ProductService) { }
 
@@ -34,15 +37,18 @@ export class AdminComponent implements OnInit {
   onEnviar2(): void {
     console.log('FORM GROUP: ', this.productForm.value);
 
-    this.productService.addProducts(this.productForm.value).subscribe(
+    this.productSubs = this.productService.addProducts(this.productForm.value).subscribe(
       res => {
-      console.log('RESP: ', res);
+        console.log('RESP: ', res);
       },
       err => {
         console.log('ERROR DE SERVIDOR');
       }
     );
+  }
 
+  ngOnDestroy(): void{
+    this.productSubs.unsubscribe();
   }
 
 }
--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../../../services/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   products = [];
+
+  productSubs: Subscription;
 
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void{
-    this.productService.getProducts().subscribe(res => {
-      
-      console.log('Respuesta 1: ', res);
-      console.log('Respuesta 2: ', Object.entries(res));
-      
+    this.productSubs = this.productService.getProducts().subscribe(res => {
+      console.log('RESPUESTA: ', res);
+      console.log('RESPUESTA: ', Object.entries(res));
+
       Object.entries(res).map(p => this.products.push(p[1]));
     });
+  }
+
+  ngOnDestroy(): void{
+     this.productSubs.unsubscribe();
   }
 
 }
