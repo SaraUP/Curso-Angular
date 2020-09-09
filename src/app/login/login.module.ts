727b14a1a93f90acf341cb5797a73ae85ab29aee
SaraUP <sarisup14@gmail.com>
2020-09-09 08:21:55 -0400

    Tarea 2: 09/08/20 --> centrear la caja de texto de estilos

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LoginRoutingModule } from './login-routing.module';
-
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { HttpClientModule } from '@angular/common/http';
-import { NgElseDirective } from '.../shared/directives/ng-else.directive';
+
 import { AuthService } from './../shared/services/auth.service';
 import { LoginComponent } from './login.component';
 import { Routes, RouterModule } from '@angular/router';
+import { NgElseDirective } from './../shared/directives/ng-else.directive';
 
 const routes: Routes = [
   {path: '', component: LoginComponent}
