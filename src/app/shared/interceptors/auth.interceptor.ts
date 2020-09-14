fd532048ad2545dbde67f6821b2a6404b58758f2
SaraUP <sarisup14@gmail.com>
2020-09-13 22:20:52 -0400

    Tarea 2: 09/11/20 --> autologout

--- src/app/shared/interceptors/auth.interceptor.ts
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -1,27 +1,51 @@
+import { Router } from '@angular/router';
+import { AuthService } from './../services/auth.service';
 import { Injectable } from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor, HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { AuthService } from '../services/auth.service';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token = this.authService.getToken();
-
-    if (token) {
+    console.log('REQUEST: ', request);
+    if (token){
       request = request.clone({
         url: request.url.indexOf('?') > -1 ? `${request.url}&auth=${token}` : `${request.url}?auth=${token}`
       });
+
     }
 
-    return next.handle(request);
+     /*if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + token
+        }
+      });
+    }*/
+
+    return next.handle(request).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 401){
+          this.handleError();
+        }
+        return throwError('Error');
+      })
+    );
   }
+
+  private handleError(): Observable<any> {
+    this.authService.logout();
+    return throwError('ERROR 401');
+  }
+
 }
